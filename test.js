dee5584eae5d41d6dae3dc5c799a529e948876f3
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-01 12:41:08 +0700

    tweaks

--- test.js
+++ test.js
@@ -4,5 +4,6 @@ const electron = require('electron');
 require('.')();
 
 electron.app.on('ready', () => {
-	(new electron.BrowserWindow({})).loadURL('https://github.com/sindresorhus/caprine/releases/tag/0.2.1');
+	(new electron.BrowserWindow())
+		.loadURL('https://github.com/sindresorhus/caprine/releases/tag/0.2.1');
 });
