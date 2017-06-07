a90eb9d00e97cf49349a496fdac93351240339e2
Kevin Martensson <kevinmartensson@gmail.com>
2017-06-07 18:18:12 +0200

    Add test for getting extension from MIME type

--- test.js
+++ test.js
@@ -6,4 +6,7 @@ require('.')();
 electron.app.on('ready', () => {
 	(new electron.BrowserWindow())
 		.loadURL('https://github.com/sindresorhus/caprine/releases/tag/0.2.1');
+
+	(new electron.BrowserWindow())
+		.loadURL(`file://${__dirname}/test.html`);
 });
