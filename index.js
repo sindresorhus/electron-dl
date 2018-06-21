47b68203ba5198f42b3dd8ff58ebd3d976c4c037
Cuong Pham <angels2it@gmail.com>
2018-06-21 15:33:46 +0700

    support multiple download

--- index.js
+++ index.js
@@ -8,138 +8,133 @@ const extName = require('ext-name');
 const {app, shell} = electron;
 
 function getFilenameFromMime(name, mime) {
-	const exts = extName.mime(mime);
+  const exts = extName.mime(mime);
 
-	if (exts.length !== 1) {
-		return name;
-	}
+  if (exts.length !== 1) {
+    return name;
+  }
 
-	return `${name}.${exts[0].ext}`;
+  return `${name}.${exts[0].ext}`;
 }
 
-function registerListener(session, options, cb = () => {}) {
-	const downloadItems = new Set();
-	let receivedBytes = 0;
-	let completedBytes = 0;
-	let totalBytes = 0;
-	const activeDownloadItems = () => downloadItems.size;
-	const progressDownloadItems = () => receivedBytes / totalBytes;
+const sessionListenerMap = new Map();
+const handlerMap = new Map();
+const downloadItems = new Set();
+let receivedBytes = 0;
+let completedBytes = 0;
+let totalBytes = 0;
+const activeDownloadItems = () => downloadItems.size;
+const progressDownloadItems = () => receivedBytes / totalBytes;
 
-	options = Object.assign({
-		showBadge: true
-	}, options);
+function registerListener(session) {
+  const listener = (e, item, webContents) => {
+    const url = decodeURIComponent(item.getURL());
+    const defaultHanlder = {
+      options: {},
+      resolve: () => {},
+      reject: () => {},
+    };
+    const { options, resolve, reject } = handlerMap.get(url) || defaultHanlder;
 
-	const listener = (e, item, webContents) => {
-		downloadItems.add(item);
-		totalBytes += item.getTotalBytes();
+    downloadItems.add(item);
+    totalBytes += item.getTotalBytes();
 
-		let hostWebContents = webContents;
-		if (webContents.getType() === 'webview') {
-			({hostWebContents} = webContents);
-		}
-		const win = electron.BrowserWindow.fromWebContents(hostWebContents);
+    let hostWebContents = webContents;
+    if (webContents.getType() === 'webview') {
+		({hostWebContents} = webContents);
+    }
+    const win = electron.BrowserWindow.fromWebContents(hostWebContents);
 
-		const dir = options.directory || app.getPath('downloads');
-		let filePath;
-		if (options.filename) {
-			filePath = path.join(dir, options.filename);
-		} else {
-			const filename = item.getFilename();
-			const name = path.extname(filename) ? filename : getFilenameFromMime(filename, item.getMimeType());
+    const dir = options.directory || app.getPath('downloads');
+    let filePath;
+    if (options.filename) {
+      filePath = path.join(dir, options.filename);
+    } else {
+      const filename = item.getFilename();
+      const name = path.extname(filename) ? filename : getFilenameFromMime(filename, item.getMimeType());
 
-			filePath = unusedFilename.sync(path.join(dir, name));
-		}
+      filePath = unusedFilename.sync(path.join(dir, name));
+    }
 
-		const errorMessage = options.errorMessage || 'The download of {filename} was interrupted';
-		const errorTitle = options.errorTitle || 'Download Error';
+    const errorMessage = options.errorMessage || 'The download of {filename} was interrupted';
+    const errorTitle = options.errorTitle || 'Download Error';
 
-		if (!options.saveAs) {
-			item.setSavePath(filePath);
-		}
+    if (!options.saveAs) {
+      item.setSavePath(filePath);
+    }
 
-		if (typeof options.onStarted === 'function') {
-			options.onStarted(item);
-		}
+    item.on('updated', () => {
+      receivedBytes = [...downloadItems].reduce((receivedBytes, item) => {
+        receivedBytes += item.getReceivedBytes();
+        return receivedBytes;
+      }, completedBytes);
 
-		item.on('updated', () => {
-			receivedBytes = [...downloadItems].reduce((receivedBytes, item) => {
-				receivedBytes += item.getReceivedBytes();
-				return receivedBytes;
-			}, completedBytes);
+      if (['darwin', 'linux'].includes(process.platform)) {
+        app.setBadgeCount(activeDownloadItems());
+      }
 
-			if (options.showBadge && ['darwin', 'linux'].includes(process.platform)) {
-				app.setBadgeCount(activeDownloadItems());
-			}
+      if (!win.isDestroyed()) {
+        win.setProgressBar(progressDownloadItems());
+      }
 
-			if (!win.isDestroyed()) {
-				win.setProgressBar(progressDownloadItems());
-			}
+      if (typeof options.onProgress === 'function') {
+        options.onProgress(progressDownloadItems());
+      }
+    });
 
-			if (typeof options.onProgress === 'function') {
-				options.onProgress(progressDownloadItems());
-			}
-		});
+    item.once('done', (e, state) => {
+      completedBytes += item.getTotalBytes();
+      downloadItems.delete(item);
 
-		item.on('done', (e, state) => {
-			completedBytes += item.getTotalBytes();
-			downloadItems.delete(item);
+      if (['darwin', 'linux'].includes(process.platform)) {
+        app.setBadgeCount(activeDownloadItems());
+      }
 
-			if (options.showBadge && ['darwin', 'linux'].includes(process.platform)) {
-				app.setBadgeCount(activeDownloadItems());
-			}
+      if (!win.isDestroyed() && !activeDownloadItems()) {
+        win.setProgressBar(-1);
+        receivedBytes = 0;
+        completedBytes = 0;
+        totalBytes = 0;
+      }
 
-			if (!win.isDestroyed() && !activeDownloadItems()) {
-				win.setProgressBar(-1);
-				receivedBytes = 0;
-				completedBytes = 0;
-				totalBytes = 0;
-			}
+      if (state === 'interrupted') {
+        const message = pupa(errorMessage, {filename: item.getFilename()});
+        electron.dialog.showErrorBox(errorTitle, message);
+        reject(new Error(message));
+      } else if (state === 'cancelled') {
+        reject(new Error('The download has been cancelled'));
+      } else if (state === 'completed') {
+        if (process.platform === 'darwin') {
+          app.dock.downloadFinished(filePath);
+        }
 
-			if (options.unregisterWhenDone) {
-				session.removeListener('will-download', listener);
-			}
+        if (options.openFolderWhenDone) {
+          shell.showItemInFolder(filePath);
+        }
 
-			if (state === 'cancelled') {
-				if (typeof options.onCancel === 'function') {
-					options.onCancel(item);
-				}
-			} else if (state === 'interrupted') {
-				const message = pupa(errorMessage, {filename: item.getFilename()});
-				electron.dialog.showErrorBox(errorTitle, message);
-				cb(new Error(message));
-			} else if (state === 'completed') {
-				if (process.platform === 'darwin') {
-					app.dock.downloadFinished(filePath);
-				}
+        resolve(item);
+      }
+      if (handlerMap.has(url)) handlerMap.delete(url);
+    });
+  };
 
-				if (options.openFolderWhenDone) {
-					shell.showItemInFolder(path.join(dir, item.getFilename()));
-				}
-
-				cb(null, item);
-			}
-		});
-	};
-
-	session.on('will-download', listener);
+  if (!sessionListenerMap.get(session)) {
+    sessionListenerMap.set(session, true);
+    session.on('will-download', listener);
+  }
 }
 
 module.exports = (options = {}) => {
-	app.on('session-created', session => {
-		registerListener(session, options);
-	});
+  app.on('session-created', session => {
+    registerListener(session, options);
+  });
 };
 
 module.exports.download = (win, url, options) => new Promise((resolve, reject) => {
 	options = Object.assign({}, options, {unregisterWhenDone: true});
 
-	registerListener(win.webContents.session, options, (err, item) => {
-		if (err) {
-			reject(err);
-		} else {
-			resolve(item);
-		}
-	});
+    handlerMap.set(decodeURIComponent(url), { options, resolve, reject });
+    registerListener(win.webContents.session);
 
-	win.webContents.downloadURL(url);
-});
+    win.webContents.downloadURL(url);
+  });
