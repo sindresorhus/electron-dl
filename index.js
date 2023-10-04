4933e1559184ea9a200ff4454b97cac68bead2ca
k2s <martin.minka@gmail.com>
2023-10-04 19:38:50 +0200

    caloing item.cancel() was not working when called onStart()

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const unusedFilename = require('unused-filename');
 const pupa = require('pupa');
 const extName = require('ext-name');
 
+class CancelError extends Error {}
+
 const getFilenameFromMime = (name, mime) => {
 	const extensions = extName.mime(mime);
 
@@ -92,10 +94,6 @@ function registerListener(session, options, callback = () => {}) {
 			item.setSavePath(filePath);
 		}
 
-		if (typeof options.onStarted === 'function') {
-			options.onStarted(item);
-		}
-
 		item.on('updated', () => {
 			receivedBytes = completedBytes;
 			for (const item of downloadItems) {
@@ -154,6 +152,7 @@ function registerListener(session, options, callback = () => {}) {
 				if (typeof options.onCancel === 'function') {
 					options.onCancel(item);
 				}
+				callback(new CancelError());
 			} else if (state === 'interrupted') {
 				const message = pupa(errorMessage, {filename: path.basename(filePath)});
 				callback(new Error(message));
@@ -183,6 +182,10 @@ function registerListener(session, options, callback = () => {}) {
 			}
 		});
 	};
+
+	if (typeof options.onStarted === 'function') {
+		options.onStarted(item);
+	}
 
 	session.on('will-download', listener);
 }
