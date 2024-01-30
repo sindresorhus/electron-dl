f8042cc724ad89135f5b23bce54122a93dcc9ace
Theo Gravity <theo@switchboard.app>
2024-01-30 14:28:24 -0800

    Fix "Save As" cancellation showing error dialog

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ function registerListener(session, options, callback = () => {}) {
 module.exports = (options = {}) => {
 	app.on('session-created', session => {
 		registerListener(session, options, (error, _) => {
-			if (error) {
+			if (error && !(error instanceof CancelError)) {
 				const errorTitle = options.errorTitle || 'Download Error';
 				dialog.showErrorBox(errorTitle, error.message);
 			}
